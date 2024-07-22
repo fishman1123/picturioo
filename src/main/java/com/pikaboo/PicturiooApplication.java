9cbe21cd43147f34cc604b3bf1071c96c851d8fa
yongjoocho <cho.y.j1997@gmail.com>
2024-07-22 17:22:51 +0900

    Initial commit

---
+++ src/main/java/com/pikaboo/PicturiooApplication.java
@@ -0,0 +1,13 @@
+package com.pikaboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicturiooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicturiooApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pikaboo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.pikaboo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PicturiooApplication.class);
+	}
+
+}
