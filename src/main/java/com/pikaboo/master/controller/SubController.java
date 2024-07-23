26792fb5f3af3a76c0f772ad5d893d1b5b78bfc8
yongjoocho <cho.y.j1997@gmail.com>
2024-07-23 09:15:31 +0900

    testing diversed controller

--- src/main/java/com/pikaboo/master/controller/SubController.java
+++ src/main/java/com/pikaboo/master/controller/SubController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class MasterController {
-    @GetMapping("/main")
+@RequestMapping("/sub")
+public class SubController {
+    @GetMapping("/")
     public String masterMain() {
-        return "index";
+        return "subTest";
     }
+
 
 }
